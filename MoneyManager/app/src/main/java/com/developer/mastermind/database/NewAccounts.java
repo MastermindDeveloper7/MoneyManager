8419a7c55df7b2db97bdfe5dc3a042de24223ae1
Vidya Shelke <vishelke@tibco.com>
2016-05-26 23:21:14 +0530

    databse files added

--- MoneyManager/app/src/main/java/com/developer/mastermind/database/NewAccounts.java
+++ MoneyManager/app/src/main/java/com/developer/mastermind/database/NewAccounts.java
@@ -1,33 +1,100 @@
 package com.developer.mastermind.database;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import java.util.ArrayList;
 
 /**
  * Created by Dhananjay on 5/9/2016.
  */
 public class NewAccounts extends SQLiteOpenHelper{
 
-    private static String DATABASE_NAME="USERS.DB";
-    private static  String TABLE_NAME="USERDATA";
-    private static String USER_NAME_COLUMN="USER_NAME";
-    private static String PASSWORD_COLUMN="PASSWORD";
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "USERS.DB";
+    private static final String TABLE_NAME = "USERDATA";
+    private static final String USER_NAME = "userName";
+    private static final String TOTAL_AMOUNT = "totalAmount";
 
-    private static String createTableQuery="Crete table " + TABLE_NAME + " ( " + USER_NAME_COLUMN + "text," + PASSWORD_COLUMN +"text );";
 
+
+
+<<<<<<< HEAD:MoneyManager/app/src/main/java/com/developer/mastermind/database/UserDatabase.java
+    public UserDatabase(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+=======
     public NewAccounts(Context context) {
         super(context,DATABASE_NAME, null, 1);
+>>>>>>> 923531e75cbda9465d98c9aecdaf87fe75ba81ce:MoneyManager/app/src/main/java/com/developer/mastermind/database/NewAccounts.java
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
 
-        db.execSQL(createTableQuery);
+        String CREATE_TABLE_QUERY = "CREATE TABLE " + TABLE_NAME + "("
+                + USER_NAME + " TEXT PRIMARY KEY," + TOTAL_AMOUNT + " INTEGER)";
+        db.execSQL(CREATE_TABLE_QUERY);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
     }
+
+    public long addUser  (String name, int amount)
+    {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(USER_NAME, name);
+        contentValues.put(TOTAL_AMOUNT, amount);
+        long numberOfRecords=db.insert(TABLE_NAME, null, contentValues);
+        return numberOfRecords;
+    }
+
+    public Integer deleteUser (String name)
+    {
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.delete(TABLE_NAME, "userName = ? ",
+                new String[] {name});
+    }
+
+    public long updateUser (String name, int amount)
+    {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(TABLE_NAME, name);
+        contentValues.put(TOTAL_AMOUNT, amount);
+        long numberOfRecords=db.update(TABLE_NAME, contentValues, "userName = ? ", new String[]{name});
+        return numberOfRecords;
+    }
+
+    public ArrayList<String> getAllUsers()
+    {
+        ArrayList<String> accountDetails = new ArrayList<String>();
+
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor res =  db.rawQuery( "select * from "+TABLE_NAME, null );
+        res.moveToFirst();
+
+        while(res.isAfterLast() == false){
+            String name=res.getString(res.getColumnIndex(USER_NAME));
+            int amount=res.getInt(res.getColumnIndex(TOTAL_AMOUNT));
+            accountDetails.add(name);
+            accountDetails.add(""+amount);
+            Log.d("Arraylist record", name + " : " + amount);
+            res.moveToNext();
+        }
+        res.close();
+        db.close();
+        return accountDetails;
+    }
+
+
+
+
 }
--- MoneyManager/app/src/main/java/com/developer/mastermind/database/UserDetails.java
+++ MoneyManager/app/src/main/java/com/developer/mastermind/database/UserDetails.java
@@ -1,25 +1,110 @@
 package com.developer.mastermind.database;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.text.format.DateFormat;
+import android.text.method.DateTimeKeyListener;
+import android.util.Log;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
 
 /**
  * Created by Dhananjay on 5/9/2016.
  */
 public class UserDetails extends SQLiteOpenHelper {
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "EXPENSES";
+    private static final String TABLE_NAME = "USERDATA";
+    private static final String USER_NAME = "userName";
+    private static final String AMOUNT = "Amount";
+    private static final String DESCRIPTION = "Description";
+    private static final String CREATED_DATE = "createdDate";
 
-    public UserDetails(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+    public UserDetails(Context context)
+    {
+        super(context,DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        String CREATE_TABLE_QUERY = "CREATE TABLE " + TABLE_NAME + "("
+                + USER_NAME + " TEXT PRIMARY KEY," + AMOUNT + " INTEGER,"+DESCRIPTION+ " TEXT,"+CREATED_DATE+" date default CURRENT_DATE)";
+        db.execSQL(CREATE_TABLE_QUERY);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS "+TABLE_NAME);
+        onCreate(db);
+    }
 
+    public long addRecord  (String name, int amount,String description)
+    {
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        Date date = new Date();
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(USER_NAME, name);
+        contentValues.put(AMOUNT, amount);
+        contentValues.put(DESCRIPTION, description);
+        contentValues.put(CREATED_DATE, dateFormat.format(date));
+        long numberOfRecords=db.insert(TABLE_NAME, null, contentValues);
+        return numberOfRecords;
+    }
+
+    public Integer deleteRecord (String name)
+    {
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.delete(TABLE_NAME, "userName = ? ",
+                new String[] {name});
+    }
+
+    public long updateRecord (String name, int amount,String description,Date date)
+    {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(TABLE_NAME, name);
+        contentValues.put(AMOUNT, amount);
+        contentValues.put(DESCRIPTION, description);
+        contentValues.put(CREATED_DATE, String.valueOf(date));
+        long numberOfRecords=db.update(TABLE_NAME, contentValues, "userName = ? ", new String[]{name});
+        return numberOfRecords;
+    }
+
+    public String getDescription(String name)
+    {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor res =  db.rawQuery("select * from " + TABLE_NAME + "where userName=" + name + "", null);
+        String description=res.getString(res.getColumnIndex(DESCRIPTION));
+        return description;
+    }
+    public ArrayList<String> getAllRecords()
+    {
+        ArrayList<String> accountDetails = new ArrayList<String>();
+
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor res =  db.rawQuery("select * from " + TABLE_NAME, null);
+        res.moveToFirst();
+
+        while(res.isAfterLast() == false){
+            String name=res.getString(res.getColumnIndex(USER_NAME));
+            int amount = res.getInt(res.getColumnIndex(AMOUNT));
+            String description=res.getString(res.getColumnIndex(DESCRIPTION));
+            String date=res.getString(res.getColumnIndex(CREATED_DATE));
+            accountDetails.add(name);
+            accountDetails.add(""+amount);
+            Log.d("Arraylist record", name + " : " + amount+" : "+description+" : "+date);
+            res.moveToNext();
+        }
+        res.close();
+        db.close();
+        return accountDetails;
     }
 }
